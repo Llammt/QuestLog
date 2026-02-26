5921c550dbae87a16baab8961aaccb0080667a58
Llammt <7nu7mumrik@gmil.com>
2026-02-26 16:28:02 +0500

    - tests: delete tests for RecyclerView;

--- app/src/main/java/com/example/questlog/TasksFragment.kt
+++ app/src/main/java/com/example/questlog/TasksFragment.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questlog.databinding.FragmentTasksBinding
 import androidx.navigation.fragment.findNavController
 import listItemAnimator
@@ -29,6 +31,7 @@ class TasksFragment : Fragment() {
         val dao = TaskDatabase.getInstance(application).taskDao
         val viewModelFactory = TasksViewModelFactory(dao)
         val viewModel = ViewModelProvider(this, viewModelFactory).get(TasksViewModel::class.java)
+
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
