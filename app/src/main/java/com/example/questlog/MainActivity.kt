d26b3f9d2e6595f343143a74288bfe5aecd3f7d0
Llammt <7nu7mumrik@gmil.com>
2026-03-03 15:42:57 +0500

    - fix: lower line in EditText in CreateTask, another layouts;

--- app/src/main/java/com/example/questlog/MainActivity.kt
+++ app/src/main/java/com/example/questlog/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.questlog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
